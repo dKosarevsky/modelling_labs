8f0dc5ec74defe3e6e75dfff441905104ac474c1
if <if>
2021-03-24 08:53:31 +0300

    add lab3 blank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from lab_001 import pseudo_random_nums
 from lab_002 import random_variables
+from lab_003 import kolmogorov
 
 st.set_page_config(initial_sidebar_state="collapsed")
 st.sidebar.image('logo.png', width=300)
@@ -8,29 +9,25 @@ st.sidebar.image('logo.png', width=300)
 
 def header():
     author = """
-        ---
         made by [Kosarevsky Dmitry](https://github.com/dKosarevsky) 
         for Modelling [labs](https://github.com/dKosarevsky/modelling_labs)
         in [BMSTU](https://bmstu.ru)
     """
     st.title("МГТУ им. Баумана. Кафедра ИУ7")
-
     st.write("Преподаватель: Рудаков И.В.")
     st.write("Студент: Косаревский Д.П.")
-    st.write("")
-    st.write("---")
     st.sidebar.markdown(author)
 
 
 def main():
     header()
-    lab = st.radio(
+    lab = st.sidebar.radio(
         "Выберите Лабораторную работу", (
             "1. Исследование последовательности псевдослучайных чисел",
             "2. Исследование случайных величин",
-            "3. :construction_worker:",
+            "3. Предельные вероятности состояний. Уравнения Колмогорова",
         ),
-        index=1
+        index=2
     )
 
     if lab[:1] == "1":
@@ -40,7 +37,8 @@ def main():
         random_variables.main()
 
     elif lab[:1] == "3":
-        st.write(":construction: Проектирование... :construction:")
+        st.write(":construction: В разработке... :construction:")
+        kolmogorov.main()
 
 
 if __name__ == "__main__":
